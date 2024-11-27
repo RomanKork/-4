e89127856160dd1b720b88dbd0419241ded6de12
User <User@WIN-CG1M31PQOK0>
2024-11-27 14:39:46 +0500

    Добавьте файлы проекта.

---
+++ лаба 4/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба_4
+{
+    public class TaskSolver
+    {
+        // Задание 1
+        public static List<int> GetUniqueElements(List<int> L1, List<int> L2)
+        {
+            var result = new List<int>();
+            foreach (var item in L1) 
+            {
+                if (!L2.Contains(item) && !result.Contains(item)) 
+                {
+                    result.Add(item); 
+                }
+            }
+            return result; 
+        }
+
+        // Задание 2
+        public static bool IsSymmetricSection(LinkedList<int> list, int i, int j)
+        {
+            var tempList = list.Skip(i).Take(j - i + 1).ToList(); 
+            int count = tempList.Count;
+            for (int k = 0; k < count / 2; k++) 
+            {
+                if (tempList[k] != tempList[count - k - 1]) 
+                    return false;
+            }
+            return true;
+        }
+
+        // Задание 3
+        public static (HashSet<string> allLike, HashSet<string> someLike, HashSet<string> noneLike) AnalyzeChocolates(
+    HashSet<string> chocolateNames, List<HashSet<string>> sweetToothPreferences)
+        {
+            var allLike = new HashSet<string>(chocolateNames);
+
+            var someLike = new HashSet<string>();
+
+            var noneLike = new HashSet<string>(chocolateNames);
+
+            foreach (var preferences in sweetToothPreferences)
+            {
+                var validPreferences = new HashSet<string>(preferences.Where(chocolateNames.Contains));
+
+                allLike.IntersectWith(validPreferences);
+                someLike.UnionWith(validPreferences);
+            }
+
+            someLike.ExceptWith(allLike);
+            noneLike.ExceptWith(someLike);
+            noneLike.ExceptWith(allLike);
+
+            return (allLike, someLike, noneLike);
+        }
+
+
+
+        // Задание 4
+        public static HashSet<char> GetMissingLettersFromFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"Файл не найден: {filePath}");
+            }
+
+            string text = File.ReadAllText(filePath);
+
+            string russianAlphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            var alphabetSet = new HashSet<char>(russianAlphabet);
+
+            var textSet = new HashSet<char>(text.ToLower().Where(char.IsLetter));
+
+            alphabetSet.ExceptWith(textSet);
+
+            return alphabetSet;
+        }
+    }
+}
---
+++ лаба 4/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace лаба_4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Задание 1
+            Console.WriteLine("Введите размер первого списка:");
+            int size1 = GetPositiveInt(); 
+            Console.WriteLine("Введите элементы первого списка:");
+            var L1 = GetIntList(size1);
+
+            Console.WriteLine("Введите размер второго списка:");
+            int size2 = GetPositiveInt();
+            Console.WriteLine("Введите элементы второго списка:");
+            var L2 = GetIntList(size2);
+
+            var result1 = TaskSolver.GetUniqueElements(L1, L2);
+            Console.WriteLine("Задание 1: Уникальные элементы: " + string.Join(", ", result1));
+
+            // Задание 2
+            Console.WriteLine("Введите размер связного списка:");
+            int linkedListSize = GetPositiveInt();
+            Console.WriteLine("Введите элементы связного списка:");
+            var linkedList = new LinkedList<int>(GetIntList(linkedListSize));
+
+            Console.WriteLine("Введите начальный индекс i:");
+            int i = NotNegativeInt();
+            Console.WriteLine("Введите конечный индекс j:");
+            int j = NotNegativeInt();
+
+            if (i >= 0 && j >= i && j < linkedListSize)
+            {
+                bool result2 = TaskSolver.IsSymmetricSection(linkedList, i, j);
+                Console.WriteLine("Задание 2: " + (result2 ? "Симметрично" : "Не симметрично"));
+            }
+            else
+            {
+                Console.WriteLine("Некорректные индексы!");
+            }
+
+            // Задание 3
+            Console.WriteLine("Введите количество названий шоколада:");
+            int chocolateCount = GetPositiveInt();
+            var chocolates = new HashSet<string>();
+
+            Console.WriteLine("Введите названия шоколада:");
+            for (int k = 0; k < chocolateCount; k++)
+            {
+                string chocolate = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(chocolate))
+                    chocolates.Add(chocolate);
+                else
+                    k--;
+            }
+
+            Console.WriteLine("Введите количество сладкоежек:");
+            int sweetToothCount = GetPositiveInt();
+            var preferences = new List<HashSet<string>>();
+
+            for (int k = 0; k < sweetToothCount; k++) 
+            {
+                Console.WriteLine($"Введите предпочтения сладкоежки {k + 1} через запятую:");
+                var input = Console.ReadLine();
+                var sweetPreferences = input.Split(',').Select(x => x.Trim()).Where(x => !string.IsNullOrWhiteSpace(x));
+                preferences.Add(new HashSet<string>(sweetPreferences));
+            }
+
+            var (allLike, someLike, noneLike) = TaskSolver.AnalyzeChocolates(chocolates, preferences);
+            Console.WriteLine("Задание 3: Всем нравятся - " + string.Join(", ", allLike));
+            Console.WriteLine("Задание 3: Некоторым нравятся - " + string.Join(", ", someLike));
+            Console.WriteLine("Задание 3: Никому не нравятся - " + string.Join(", ", noneLike));
+
+            // Задание 4
+            var missingLetters = TaskSolver.GetMissingLettersFromFile("C:\\Users\\User\\Desktop\\textfile.txt");
+            try
+            {
+                string fileContent = File.ReadAllText("C:\\Users\\User\\Desktop\\textfile.txt");
+                Console.WriteLine("Текст из файла:");
+                Console.WriteLine(fileContent);
+            }
+            catch {
+                Console.WriteLine("Файл не получилось открыть.");
+            }
+
+            Console.WriteLine("Отсутствующие буквы:");
+            Console.WriteLine(string.Join(", ", missingLetters));
+        }
+
+        public static int NotNegativeInt()
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int result) && result >= 0)
+                    return result;
+                Console.WriteLine("Некорректный ввод. Введите неотрицательное число.");
+            }
+        }
+        public static int GetPositiveInt()
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int result) && result > 0)
+                    return result;
+                Console.WriteLine("Некорректный ввод. Введите положительное число.");
+            }
+        }
+
+        public static List<int> GetIntList(int size)
+        {
+            var list = new List<int>();
+            while (list.Count < size)
+            {
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out int number))
+                    list.Add(number);
+                else
+                    Console.WriteLine("Некорректный ввод. Введите целое число.");
+            }
+            return list;
+        }
+    }
+}
